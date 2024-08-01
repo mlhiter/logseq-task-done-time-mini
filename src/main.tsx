712dbcf670718c9aaacf3af7323adca2a83116c4
Alex Lee <3076438032@qq.com>
2024-08-01 23:21:35 +0800

    feat: the other two display modes

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import App from './App'
 import { getDatePattern } from './libs/date'
 import { settingSchema } from './libs/settings'
 import { logseq as pluginInfo } from '../package.json'
+import { BlockEntity, BlockIdentity } from '@logseq/libs/dist/LSPlugin'
 
 // @ts-expect-error
 const css = (t, ...args) => String.raw(t, ...args)
@@ -91,11 +92,29 @@ async function main() {
     const block = await logseq.Editor.getBlock(data.blocks[0].uuid)
     const isDoneStatus = block?.marker === 'DONE'
 
-    if (!block || !block.content) return
+    if (!block || !block.content || !block.marker) return
 
     const { doneContent, displayMode, displayPosition, collapseMode } =
       logseq.settings as unknown as Settings
 
+    switch (displayMode) {
+      case 'content':
+        await updateContent(block, isDoneStatus, doneContent, displayPosition)
+        break
+      case 'property':
+        await updateProperty(block, isDoneStatus, doneContent)
+        break
+      case 'childBlock':
+        await updateChildBlock(block, isDoneStatus, doneContent, collapseMode)
+        break
+    }
+  })
+  async function updateContent(
+    block: BlockEntity,
+    isDoneStatus: boolean,
+    doneContent: string,
+    displayPosition: string
+  ) {
     const datePattern = getDatePattern(preferredDateFormat)
     const timePattern = '\\d{2}:\\d{2}(:\\d{2})?'
     // 转义 doneContent 中的特殊字符
@@ -107,12 +126,9 @@ async function main() {
       .replace(/\\\{date\\\}/g, datePattern)
       .replace(/\\\{time\\\}/g, timePattern)
     const regex = new RegExp(combinedPattern)
-    console.log(regex.test(block.content))
 
     // 1. 已经加入过内容，而且状态为DONE，则不操作
-    if (regex.test(block.content) && isDoneStatus) {
-      return
-    }
+    if (regex.test(block.content) && isDoneStatus) return
 
     // 2. 已经加入过内容，但是状态变为其他状态，删除添加的内容
     if (regex.test(block.content)) {
@@ -136,10 +152,112 @@ async function main() {
         ? `DONE ${contentWithoutDone} ${tempContentForReplace}`
         : `DONE ${tempContentForReplace} ${contentWithoutDone}`
 
-    if (isDoneStatus) {
-      await logseq.Editor.updateBlock(block.uuid, finalContentForReplace)
+    await logseq.Editor.updateBlock(block.uuid, finalContentForReplace)
+  }
+  async function updateProperty(
+    block: BlockEntity,
+    isDoneStatus: boolean,
+    doneContent: string
+  ) {
+    // 从doneContent中提取出属性值
+    const propertyRegex = /(\w+)::\s+(.+)/
+    const match = doneContent.match(propertyRegex)
+    if (!match) {
+      await logseq.UI.showMsg('请在设置中设置正确的属性格式.', 'error')
+      return
     }
-  })
+    const propertyName = match[1]
+    const propertyValue = match[2]
+    console.log(propertyName, propertyValue)
+    const hasProperty = Object.prototype.hasOwnProperty.call(
+      block.properties,
+      propertyName
+    )
+
+    // 1. 已经添加过相应属性，而且状态为DONE，则不操作
+    if (hasProperty && isDoneStatus) return
+
+    // 2. 已经添加过相应属性，但是状态变为其他状态，删除添加的属性
+    if (hasProperty) {
+      await logseq.Editor.removeBlockProperty(block.uuid, propertyName)
+      return
+    }
+
+    // 3. 没有添加过相应属性，而且状态不为DONE则不操作
+    if (!isDoneStatus) return
+
+    // 4. 没有添加过相应属性，而且状态为DONE则添加属性
+    const finalPropertyValue = propertyValue
+      .replace(/\{date\}/g, format(new Date(), preferredDateFormat))
+      .replace(/\{time\}/g, format(new Date(), 'HH:mm'))
+
+    await logseq.Editor.upsertBlockProperty(
+      block.uuid,
+      propertyName,
+      finalPropertyValue
+    )
+  }
+  async function updateChildBlock(
+    block: BlockEntity,
+    isDoneStatus: boolean,
+    doneContent: string,
+    collapseMode: boolean
+  ) {
+    const datePattern = getDatePattern(preferredDateFormat)
+    const timePattern = '\\d{2}:\\d{2}(:\\d{2})?'
+    // 转义 doneContent 中的特殊字符
+    const escapeRegExp = (string: string) => {
+      return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+    }
+    // 替换 doneContent 中的占位符，并保留转义后的特殊字符
+    // 特殊情况：插入的子块的开头的空白字符会被忽略，所以需要把这里的空白字符也忽略
+    const combinedPattern = escapeRegExp(doneContent)
+      .replace(/\\\{date\\\}/g, datePattern)
+      .replace(/\\\{time\\\}/g, timePattern)
+      .trim()
+    const regex = new RegExp(combinedPattern)
+    // 特殊情况：这里要提前判断是否有子块，否则firstChild拿不到会报错
+    if (block.children?.length === 0 && isDoneStatus) {
+      const contentForChild = doneContent
+        .replace(/\{date\}/g, format(new Date(), preferredDateFormat))
+        .replace(/\{time\}/g, format(new Date(), 'HH:mm'))
+
+      await logseq.Editor.insertBlock(block.uuid, contentForChild)
+      if (collapseMode) {
+        await logseq.Editor.setBlockCollapsed(block.uuid, true)
+      }
+      return
+    }
+    if (block.children?.length === 0 && !isDoneStatus) return
+
+    const firstChildUUid = block.children?.[0]?.[1] as BlockIdentity
+    const firstChild = await logseq.Editor.getBlock(firstChildUUid)
+    const hasDoneContent = regex.test(firstChild!.content)
+
+    // 1. 没有加入过相应子块，而且状态不为DONE，则不操作
+    if (!isDoneStatus && !hasDoneContent) return
+
+    // 2. 已经加入过相应子块，而且状态为DONE，则不操作
+    if (hasDoneContent && isDoneStatus) return
+
+    // 3. 已经加入过相应子块，但是状态变为其他状态，删除添加的子块
+    if (hasDoneContent) {
+      await logseq.Editor.removeBlock(firstChildUUid)
+      return
+    }
+    // 4. 没有加入过相应子块，而且状态为DONE则加入子块
+    // 首先检测doneContent是否包含{date}，{time}等变量并分别替换为真实值
+    if (!hasDoneContent && isDoneStatus) {
+      const contentForChild = doneContent
+        .replace(/\{date\}/g, format(new Date(), preferredDateFormat))
+        .replace(/\{time\}/g, format(new Date(), 'HH:mm'))
+
+      await logseq.Editor.insertBlock(block.uuid, contentForChild)
+      if (collapseMode) {
+        await logseq.Editor.setBlockCollapsed(block.uuid, true)
+      }
+    }
+  }
 }
 
 logseq.ready(main).catch(console.error)
